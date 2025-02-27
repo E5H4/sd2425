e32f557525893ed800c44102c363dcfe46f60cdb
Esha <114303966+E5H4@users.noreply.github.com>
2025-02-26 21:11:45 -0500

    esha's UI added
    > basic dialogue testing added
    > sprite added

---
+++ Assets/Our Scripts/Esha's Scripts/TextPlsWork.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DialogueSystemWithText;
+
+public class TextPlsWork : MonoBehaviour
+{
+    [SerializeField] private DialogueUIController _dialogueUIController; // this is my start dialogue test (DIALOGUEUI) , premade and prenamed
+     [SerializeField] private DialogueUIController minordialoguecontroller;
+    [SerializeField] private DialogueUIController severedialoguecontroller;
+    [SerializeField] private GameObject StartDialogue;  // Reference to the StartDialogue object
+    [SerializeField] private GameObject MinorDialogue;  // Reference to the MinorDialogue object
+    [SerializeField] private GameObject SevereDialogue;  // Reference to the SevereDialogue object
+
+
+    private void Start()
+    {
+        if (_dialogueUIController != null)
+        {
+            Debug.Log("Starting Dialogue...");
+            _dialogueUIController.ShowDialogueUI(); // This will show the dialogue panel
+        }
+        else
+        {
+            Debug.LogError("DialogueUIController is not assigned.");
+        }
+    }
+
+    public void MinorDifficulty() // minor/female version
+    {
+        // Disable StartDialogue
+        if (StartDialogue != null)
+        {
+            StartDialogue.SetActive(false);
+        }
+
+        // Enable MinorDialogue
+        if (MinorDialogue != null)
+        {
+            MinorDialogue.SetActive(true);
+        }
+
+        // Call new dialogue system
+        minordialoguecontroller.ShowDialogueUI();
+
+        Debug.Log("Minor difficulty mode activated.");
+    }
+
+    public void MajorDifficulty() //severe/male version
+    {
+        // Disable StartDialogue
+        if (StartDialogue != null)
+        {
+            StartDialogue.SetActive(false);
+        }
+
+        // Enable SevereDialogue
+        if (SevereDialogue != null)
+        {
+            SevereDialogue.SetActive(true);
+        }
+
+        // Call new dialogue system
+        severedialoguecontroller.ShowDialogueUI();
+
+        Debug.Log("Major difficulty mode activated.");
+    }
+
+}
