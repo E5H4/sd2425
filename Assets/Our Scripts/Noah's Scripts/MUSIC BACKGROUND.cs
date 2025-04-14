b6a42c184ca5b337c9a2387fe64f863221cb8ef6
Nfine0106 <2796836@vikes.csuohio.edu>
2025-04-14 02:22:23 -0400

    ALMOST DONE
    Recorded voice lines, Gonna wrap up buttons and make the vocal triggers happen. Also imported a UI asset

---
+++ Assets/Our Scripts/Noah's Scripts/MUSIC BACKGROUND.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MUSICBACKGROUND : MonoBehaviour
+{
+    /*
+    [SerializeField] private AudioSource backgroundAudio;
+    [SerializeField] private AudioClip sceneClip; 
+
+    private void Start()
+    {
+        if (backgroundAudio == null)
+        {
+            backgroundAudio = GetComponent<AudioSource>();
+            if (backgroundAudio == null)
+            {
+                backgroundAudio = gameObject.AddComponent<AudioSource>();
+            }
+        }
+
+        if (sceneClip != null)
+        {
+            backgroundAudio.clip = sceneClip;
+            backgroundAudio.loop = true;
+            backgroundAudio.volume = 0.064f;
+            backgroundAudio.Play();
+            Debug.Log("bg music playing");
+        }
+
+    }
+    */
+}
